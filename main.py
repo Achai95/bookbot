c8b60db3b36275cc7bfd0e681b28211fb0d193c3
Achai <kkonar58@gmail.com>
2025-01-14 17:25:59 +0100

    added get report

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ def main():
     book_path= "books/frankenstein.txt"
     text = get_book_text(book_path)
     count= get_word_count(text)
-    letter= get_letter_count(text)
-    print(letter)
+    letters= get_letter_count(text)
+    alpha= get_alphabet(letters)
+    get_report(alpha,count)
 
 
 def get_book_text(path):
@@ -30,12 +31,23 @@ def get_letter_count(text):
     
     return count
 
-def get_report(letter_count):
+def get_alphabet(letter_count):
    letters=letter_count
+   report= {}
    for letter in letters:
-        
-    
+        if letter.isalpha()== True:
+            report[letter] = letters[letter]
+   return report
 
+def get_report(dict,word):
+    letters=dict
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(word," words found in the document")
+    print(" ")
+    for letter in letters:
+        print("the '",letter,"' character was found ",letters[letter]," times")
+    
     return
+    
 
 main()
